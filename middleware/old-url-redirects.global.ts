0e938f1e4bed7ad1e595afc6194513487205d2e9
Andrew Rovniy <smart.fz@mail.ru>
2024-10-24 13:45:21 +0300

    add /boo-stories-privacy-policy/ redirect

--- middleware/old-url-redirects.global.ts
+++ middleware/old-url-redirects.global.ts
@@ -2,6 +2,7 @@ export default defineNuxtRouteMiddleware((to, _) => {
   const redirects = {
     '/projects/tabs-broadcast/': '/blogs/tabs-broadcast',
     '/projects/boo-stories/': '/categories/tinyboo',
+    '/boo-stories-privacy-policy/': '/docs/tiny-boo-privacy-policy',
   }
 
   // Проверяем, нужно ли делать редирект
